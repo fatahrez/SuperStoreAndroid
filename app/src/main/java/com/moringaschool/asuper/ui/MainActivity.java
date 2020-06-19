9589fa5e039710a8d4f5b1b120935a46f3aeff2d
Nyawira Muturi <albinamuturi@gmail.com>
2020-06-20 02:22:10 +0300

    add animations

--- app/src/main/java/com/moringaschool/asuper/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/asuper/ui/MainActivity.java
@@ -3,14 +3,27 @@ package com.moringaschool.asuper.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.moringaschool.asuper.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variables
+    Animation topAnim;
+    ImageView shop, checklist;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim= AnimationUtils.loadAnimation(this, R.anim.top_animation);
     }
 }
