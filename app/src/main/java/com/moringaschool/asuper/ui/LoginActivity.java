6a91588884d4ef4a08b487aa6642b1b10c58cdeb
Nyawira Muturi <albinamuturi@gmail.com>
2020-06-23 13:08:13 +0300

    complete login and sign up

--- app/src/main/java/com/moringaschool/asuper/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/asuper/ui/LoginActivity.java
@@ -2,17 +2,33 @@ package com.moringaschool.asuper.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.moringaschool.asuper.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button signUp;
+    Button login;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        signUp = findViewById(R.id.signUp);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (LoginActivity.this, Signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
