240a71732405c207b650beaa9810d424a967037f
Nyawira Muturi <albinamuturi@gmail.com>
2020-06-23 11:41:52 +0300

    add login page

---
+++ app/src/main/java/com/moringaschool/asuper/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.asuper.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.asuper.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
