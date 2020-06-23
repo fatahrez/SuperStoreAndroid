c3c3a1ea26406f775a9a7e4c18c9f65a20f59ae8
Nyawira Muturi <albinamuturi@gmail.com>
2020-06-23 13:25:56 +0300

    make final changes to login forms

--- app/src/main/java/com/moringaschool/asuper/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/asuper/ui/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.moringaschool.asuper.R;
 
@@ -14,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
 
     Button signUp;
     Button login;
+    EditText mEmail, mPassword;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,27 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signUp = findViewById(R.id.signUp);
+        login = findViewById(R.id.login);
+        mEmail = findViewById(R.id.emailAddress);
+        mPassword = findViewById(R.id.password);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (LoginActivity.this, Signup.class);
                 startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAddress = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+                intent.putExtra("email", emailAddress);
+                intent.putExtra("password", password);
+                startActivity(intent);
+                finish();
             }
         });
     }
