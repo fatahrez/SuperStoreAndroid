979734995a926cfd89281adf47b74ba26c1550e0
Nyawira Muturi <albinamuturi@gmail.com>
2020-06-25 00:24:08 +0300

    make final changes to dashboard and navigation bar

--- app/src/main/java/com/moringaschool/asuper/ui/AdminActivity.java
+++ app/src/main/java/com/moringaschool/asuper/ui/AdminActivity.java
@@ -1,4 +1,42 @@
 package com.moringaschool.asuper.ui;
 
-public class AdminActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
+import com.moringaschool.asuper.R;
+
+
+public class AdminActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView mImage;
+    NavigationView mNavigation;
+    DrawerLayout drawerLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_admin);
+
+        drawerLayout= findViewById(R.id.drawerLayout);
+        mImage= findViewById(R.id.menuImg);
+        mImage.setOnClickListener(this);
+
+        }
+
+    @Override
+    public void onClick(View v) {
+        drawerLayout.openDrawer(GravityCompat.START);
+
+    }
 }
