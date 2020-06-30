56f79a7a022e87b94b2b9dab1ca874419b73754c
Nyawira Muturi <albinamuturi@gmail.com>
2020-06-30 13:38:12 +0300

    add admin object

--- app/src/main/java/com/moringaschool/asuper/ServiceGenerator.java
+++ app/src/main/java/com/moringaschool/asuper/ServiceGenerator.java
@@ -1,5 +1,5 @@
 package com.moringaschool.asuper;
 
-public interface SuperApi {
+public class ServiceGenerator {
 
 }
---
+++ app/src/main/java/com/moringaschool/asuper/models/Admin.java
@@ -0,0 +1,34 @@
+package com.moringaschool.asuper.models;
+
+public class Admin {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String emailAddress;
+    private String token;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
