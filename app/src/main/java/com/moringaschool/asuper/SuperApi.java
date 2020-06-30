ccf237d41d037e4f4e24c9cbe984dbedee36d053
Nyawira Muturi <albinamuturi@gmail.com>
2020-06-30 13:50:13 +0300

    add API interface

--- app/src/main/java/com/moringaschool/asuper/SuperApi.java
+++ app/src/main/java/com/moringaschool/asuper/SuperApi.java
@@ -1,5 +1,19 @@
 package com.moringaschool.asuper;
 
-public interface SuperApi {
+import com.moringaschool.asuper.models.Admin;
+import com.moringaschool.asuper.models.Login;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface SuperApi {
+    @POST ("login")
+    Call<Admin> login(@Body Login login);
+
+    @GET("secretInfo")
+    Call<ResponseBody> getSecret(@Header("Authorization") String authToken);
 }
---
+++ app/src/main/java/com/moringaschool/asuper/models/Login.java
@@ -0,0 +1,6 @@
+package com.moringaschool.asuper.models;
+
+public class Login {
+    private String email;
+    private String password;
+}
