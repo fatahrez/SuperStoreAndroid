c4a106007755c78a3e2d7668297c327bfc1d9ca1
Nyawira Muturi <albinamuturi@gmail.com>
2020-07-01 14:18:22 +0300

    add recycler view

--- app/src/main/java/com/moringaschool/asuper/accounts/Adding.java
+++ app/src/main/java/com/moringaschool/asuper/accounts/Adding.java
@@ -1,18 +1,31 @@
 package com.moringaschool.asuper.accounts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.moringaschool.asuper.R;
+import com.moringaschool.asuper.models.ClerkItem;
+
+import java.util.ArrayList;
 
 public class Adding extends AppCompatActivity implements View.OnClickListener{
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding);
+
+        ArrayList<ClerkItem> clerkList = new ArrayList<>();
+        clerkList.add(new ClerkItem(R.drawable.professionals, "Shop 1", "Clerk 1"));
+        clerkList.add(new ClerkItem(R.drawable.teacher, "Shop 2", "Clerk 2"));
+        clerkList.add(new ClerkItem(R.drawable.woman, "Shop 3", "Clerk 3"));
     }
 
     @Override
---
+++ app/src/main/java/com/moringaschool/asuper/models/ClerkItem.java
@@ -0,0 +1,25 @@
+package com.moringaschool.asuper.models;
+
+public class ClerkItem {
+    private int mImageResource;
+    private String mText1;
+    private String mText2;
+
+    public ClerkItem(int mImageResource, String mText1, String mText2) {
+        this.mImageResource = mImageResource;
+        this.mText1 = mText1;
+        this.mText2 = mText2;
+    }
+
+    public int getmImageResource() {
+        return mImageResource;
+    }
+
+    public String getmText1() {
+        return mText1;
+    }
+
+    public String getmText2() {
+        return mText2;
+    }
+}
