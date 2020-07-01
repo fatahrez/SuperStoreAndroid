1875cb7065572de69c04b922d58f6e5053074493
Nyawira Muturi <albinamuturi@gmail.com>
2020-07-01 12:44:38 +0300

    add onclick listeners to cardviews

---
+++ app/src/main/java/com/moringaschool/asuper/accounts/Adding.java
@@ -0,0 +1,22 @@
+package com.moringaschool.asuper.accounts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.moringaschool.asuper.R;
+
+public class Adding extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/moringaschool/asuper/ui/AdminActivity.java
+++ app/src/main/java/com/moringaschool/asuper/ui/AdminActivity.java
@@ -12,8 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.navigation.NavigationView;
 import com.moringaschool.asuper.R;
+import com.moringaschool.asuper.accounts.Adding;
 
 
 public class AdminActivity extends AppCompatActivity implements View.OnClickListener {
@@ -21,6 +23,7 @@ public class AdminActivity extends AppCompatActivity implements View.OnClickList
     ImageView mImage;
     NavigationView mNavigation;
     DrawerLayout drawerLayout;
+    MaterialCardView mReports, mSales, mProducts, mClerks;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,12 +34,19 @@ public class AdminActivity extends AppCompatActivity implements View.OnClickList
         drawerLayout= findViewById(R.id.drawerLayout);
         mImage= findViewById(R.id.menuImg);
         mImage.setOnClickListener(this);
-
+        mReports.setOnClickListener(this);
+        mSales.setOnClickListener(this);
+        mProducts.setOnClickListener(this);
+        mClerks.setOnClickListener(this);
         }
 
     @Override
     public void onClick(View v) {
         drawerLayout.openDrawer(GravityCompat.START);
 
+        if (v == mClerks) {
+            Intent intent = new Intent(AdminActivity.this, Adding.class);
+            startActivity(intent);
+        }
     }
 }
