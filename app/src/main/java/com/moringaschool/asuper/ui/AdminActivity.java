c73fc4fc7378816ce5a6ef1a62ba6af7370522f4
Nyawira Muturi <albinamuturi@gmail.com>
2020-07-02 01:16:39 +0300

    add custom graph

--- app/src/main/java/com/moringaschool/asuper/ui/AdminActivity.java
+++ app/src/main/java/com/moringaschool/asuper/ui/AdminActivity.java
@@ -3,6 +3,9 @@ package com.moringaschool.asuper.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +19,7 @@ import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.navigation.NavigationView;
 import com.moringaschool.asuper.R;
 import com.moringaschool.asuper.accounts.Adding;
+import com.moringaschool.asuper.report.ReportsActivity;
 
 
 public class AdminActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,6 +35,7 @@ public class AdminActivity extends AppCompatActivity implements View.OnClickList
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_admin);
 
+        mNavigation = findViewById(R.id.navigationView);
         drawerLayout= findViewById(R.id.drawerLayout);
         mImage= findViewById(R.id.menuImg);
         mReports= findViewById(R.id.reports);
@@ -45,12 +50,19 @@ public class AdminActivity extends AppCompatActivity implements View.OnClickList
         mProducts.setOnClickListener(this);
         mClerks.setOnClickListener(this);
 
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(mNavigation, navController);
+
         }
 
     @Override
     public void onClick(View v) {
         if (v == mClerks) {
             Intent intent = new Intent(AdminActivity.this, Adding.class);
+            startActivity(intent);
+        }
+        if (v == mReports) {
+            Intent intent = new Intent(AdminActivity.this, ReportsActivity.class);
             startActivity(intent);
         }
     }
